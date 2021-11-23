bfba8587ff6233dd6d2f34b3c703c99d5142cddf
9908229348 <kalavaisravanthi98@gmail.com>
2021-11-23 08:17:01 +0530

    Added: Test that checks number of records matched or not

---
+++ test/java/addressbook/EmployeePayRollTest.java
@@ -0,0 +1,17 @@
+package addressbook;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class EmployeePayRollTest {
+
+    @Test
+    public void givenDataBaseConnection_ReturnDataFromDataBase(){
+        EmployeePayRollService employeePayRollService = new EmployeePayRollService();
+        List<PayRollData> list = employeePayRollService.readEmployeePayRoll();
+        System.out.println(list);
+        Assert.assertEquals(3, list.size());
+    }
+}
